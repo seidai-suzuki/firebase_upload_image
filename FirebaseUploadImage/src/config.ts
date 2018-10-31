1e8a5464839528d7aab44160edd0ea2eb0bee041
Your Name <you@example.com>
2018-10-31 22:25:26 +0900

    設定情報追加

---
+++ FirebaseUploadImage/src/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    fireSetting: {
+      apiKey: "AIzaSyC59TNuTl1VMwfc3qQSIYNDpKF2JxwtmMk",
+      authDomain: "uploadimage-f5ed3.firebaseapp.com",
+      databaseURL: "https://uploadimage-f5ed3.firebaseio.com",
+      projectId: "uploadimage-f5ed3",
+      storageBucket: "uploadimage-f5ed3.appspot.com",
+      messagingSenderId: "686577671910"
+    }
+};
